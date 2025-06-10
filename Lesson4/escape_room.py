21171db04e92d2d873d322a69477415f118ecbea
barbaragrcast <barbaragrcast@gmail.com>
2025-06-10 20:36:24 +0000

    Improved the assignment

--- Lesson4/escape_room.py
+++ Lesson4/escape_room.py
@@ -1,17 +1,25 @@
 name = input("What is your name? ")
 
 print(f'Welcome to our escape room, {name}!')
-Door_choice = input('Before you stand three doors: Door 1, Door 2,' \
-'and Door 3. Which one will you choose? ')
 
-if Door_choice == 'Door 1':
-    print("Congrats! you escaped")
+def game():
+    Door_choice = input('Before you stand three doors: Door 1, Door 2,' \
+    'and Door 3. Which one will you choose? ')
+    if Door_choice == 'Door 1':
+        print("Congrats! you escaped")
 
-if Door_choice == 'Door 2':
-    print('There was nothing behind the door, you lost' )
+    elif Door_choice == 'Door 2':
+        print('There was nothing behind the door, you lost' )
+        try_again = input('Want to try again? Type yes or no: ')
+        if try_again == 'yes':
+            game()
+            
+    elif Door_choice == 'Door 3':
+        print('There was nothing behind the door, you lost' )
+        try_again = input('Want to try again? Type yes or no: ')
+        if try_again == 'yes':
+            game()
+    else:
+        print('Please only type: Door 1, Door 2 or Door 3')
+game()
 
-if Door_choice == 'Door 3':
-    print('There was nothing behind the door, you lost' )
-
-else:
-    print('Please only type: Door 1, Door 2 or Door 3')
